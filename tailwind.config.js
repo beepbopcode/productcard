4f818e764598d529bb5343f27a4369344d8680ff
Emir Alp Acar <114298224+beepbopcode@users.noreply.github.com>
2023-09-07 13:51:53 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans'],
+      serif: ['Fraunces', 'serif'],
+    },
+    extend: {
+      colors: {
+        darkcyan: 'hsl(158, 36%, 37%)',
+        cream: 'hsl(30, 38%, 92%)',
+        verydarkblue: 'hsl(212, 21%, 14%)',
+        darkgrayishblue: 'hsl(228, 12%, 48%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+    },
+  },
+  
+  plugins: [],
+}
